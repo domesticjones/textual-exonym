7811a6c529de5a35fc4504a9db976225e3880822
Dustin Jones <info@domesticjones.com>
2018-06-07 09:30:23 -0600

    Create page hero image module, style header, contact form, and hero image for mobile/tablet

---
+++ site/web/app/themes/exonym/templates/module-header.php
@@ -0,0 +1,21 @@
+<?php
+  $headerType = get_field('header_type');
+  $header = get_field('header_content');
+  $headerImage = $header['image'];
+  $headerData = $header['data'];
+?>
+<header class="page-header animate-parallax animate-z-extreme header-height-<?php echo $headerType; ?>">
+  <?php if(!empty($headerImage)) { echo '<div class="module-bg" style="background-image: url(' . $headerImage['url'] . ')"></div>'; } ?>
+    <section class="header-content">
+      <?php if(!empty($headerData['headline'])) { echo '<h1>' . $headerData['headline'] . '</h1>'; } ?>
+      <?php if(!empty($headerData['description'])) { echo '<p>' . $headerData['description'] . '</p>'; } ?>
+      <?php if(!empty($headerData['button'])): ?>
+        <a href="<?php echo $headerData['button']['url']; ?>">
+          <button type="button">
+            <span><?php echo $headerData['button']['title']; ?></span>
+            <span class="button-arrow"><?php echo file_get_contents(asset_path('images/icon-arrow.svg')); ?></span>
+          </button>
+        </a>
+      <?php endif; ?>
+    </section>
+</header>
---
+++ site/web/app/themes/exonym/templates/page-home.php
@@ -0,0 +1,13 @@
+<?php
+	/* ===================
+		 TEMPLATE NAME: Home
+		 =================== */
+	get_header();
+	get_template_part('templates/wrap', 'start');
+	get_template_part('templates/module', 'header');
+?>
+
+<?php
+	get_template_part('templates/wrap', 'end');
+	get_footer();
+?>
