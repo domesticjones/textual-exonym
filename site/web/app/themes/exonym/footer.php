54ebae2daa310916400488001bfeba267cd02212
Dustin Jones <info@domesticjones.com>
2018-06-07 21:24:16 -0600

    Style global footer

--- site/web/app/themes/exonym/footer.php
+++ site/web/app/themes/exonym/footer.php
@@ -3,25 +3,31 @@
      DEFAULT FOOTER
      ============== */
 ?>
-    <footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-      <div class="wrap">
-        <p class="copyright">&copy; Copyright <?php ex_brand('legal'); ?></p>
-        <nav class="nav-footer" role="navigation">
-          <?php wp_nav_menu(array(
-            'container' => 'ul',                    // enter '' to remove nav container
-            'container_class' => 'footer-links cf',	// class of container (should you choose to use it)
-            'menu' => __('Footer', 'exonym'),	      // nav name
-            'menu_class' => 'nav footer-nav cf',    // adding custom nav class
-            'theme_location' => 'footer-menu',		  // where it's located in the theme
-            'before' => '',							            // before the menu
-            'after' => '',							            // after the menu
-            'link_before' => '',					          // before each link
-            'link_after' => '',						          // after each link
-            'depth' => 1,							              // limit the depth of the nav
-            'fallback_cb' => ''						          // fallback function
-          )); ?>
-        </nav>
-      </div>
+    <footer id="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+      <a href="<?php echo get_home_url(); ?>" class="footer-logo">
+        <?php
+          $logoFile = get_field('logo', 'options')['primary']['full_color']['url'];
+          echo file_get_contents($logoFile);
+        ?>
+      </a>
+      <nav class="nav-footer" role="navigation">
+        <h2>About <?php echo ex_brand(); ?></h2>
+        <?php wp_nav_menu(array(
+          'container' => 'ul',                    // enter '' to remove nav container
+          'container_class' => 'footer-links cf',	// class of container (should you choose to use it)
+          'menu' => __('Footer', 'exonym'),	      // nav name
+          'menu_class' => 'nav footer-nav cf',    // adding custom nav class
+          'theme_location' => 'footer-menu',		  // where it's located in the theme
+          'before' => '',							            // before the menu
+          'after' => '',							            // after the menu
+          'link_before' => '',					          // before each link
+          'link_after' => '',						          // after each link
+          'depth' => 1,							              // limit the depth of the nav
+          'fallback_cb' => ''						          // fallback function
+        )); ?>
+      </nav>
+      <?php ex_social(); ?>
+      <p class="copyright">&copy; Copyright <?php ex_brand('legal'); ?></p>
     </footer>
   </div>
   <?php wp_footer(); ?>
